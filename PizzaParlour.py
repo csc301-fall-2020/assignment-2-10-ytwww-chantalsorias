8dfda78f818009768cd3d23a080063291d6c0f76
Chantal Sorias <chantal@Chantals-MacBook-Pro.local>
2020-11-05 19:07:20 -0500

    driver: Chantal. Remove item, cancel order

--- PizzaParlour.py
+++ PizzaParlour.py
@@ -5,8 +5,8 @@ from MenuItem import Menu
 from MenuItem import CustomPizza
 from MenuItem import Size
 from MenuItem import Topping
-from Order import OrderItem
-from Order import Orders
+from Order import OrderItem, Orders
+from Prices import drinkPrices, toppingPrices, pizzaPrices
 
 app = Flask("Assignment 2")
 
@@ -39,19 +39,6 @@ menu.add_drink(dietpepsi)
 menu.add_drink(coke)
 
 
-def generate_new_order_number():
-    global order_number
-    new_order_number = order_number
-    order_number += 1
-    return str(new_order_number)
-
-
-def merge_two_dicts(x, y):
-    z = x.copy()
-    z.update(y)
-    return z
-
-
 # routes
 @app.route('/pizza')
 def welcome_pizza():
@@ -60,18 +47,9 @@ def welcome_pizza():
 
 @app.route('/menu')
 def get_menu_items():
-    # pizzas = get_pizzas()
-    # drinks = get_drinks()
-    # menu = merge_two_dicts(pizzas, drinks)
-    # return menu
     return menu.get_menu_items()
 
 
-# @app.route('/menu/<menu_item_type>/<item_name>')
-# def get_menu_item(menu_item_type, item_name):
-    # for item in menu_items[menu_item_type]:
-    #     if item.name == item_name:
-    #         return str(item.price)
 @app.route('/menu/pizzas/<pizza_name>')
 def get_pizza_price(pizza_name):
     return str(menu.find_pizza(pizza_name).price)
@@ -90,19 +68,11 @@ def get_topping_price(topping_name):
 @app.route('/menu/pizzas')
 def get_pizzas():
     return menu.get_pizzas()
-    # pizzas = {}
-    # for item in menu_items["pizzas"]:
-    #     pizzas[item.name] = item.price
-    # return pizzas
 
 
 @app.route('/menu/drinks')
 def get_drinks():
     return menu.get_drinks()
-    # drinks = {}
-    # for item in menu_items["drinks"]:
-    #     drinks[item.name] = item.price
-    # return drinks
 
 # order routes
 
@@ -111,108 +81,92 @@ def get_drinks():
 def get_toppings():
     return menu.get_toppings()
 
+# Create a new order
+
 
 @app.route('/order')
 def new_order():
     new_order = orders.new_order()
-    # orders.add_order(new_order)
     return "Your order number is " + str(new_order.order_number)
-    # # return generate_new_order_number()
 
 
 @app.route('/order/<order_number>', methods=['GET', 'DELETE'])
 def get_order(order_number):
     order = orders.find_order(int(order_number))
+    # Check if order exists
     if isinstance(order, str):
         return order
+    # If GET, display items in order
     if request.method == 'GET':
         return order.display_items()
-        # items_in_order = {}
-        # for item in order_items:
-        #     if item.order_number == order_number:
-        #         if item.item.name in items_in_order:
-        #             items_in_order[item.item.name]["quantity"] += 1
-        #         else:
-        #             items_in_order[item.item.name] = {
-        #                 "price": item.item.price, "quantity": 1}
-
-        # return items_in_order
-
+    # If DELETE, delete order
     if request.method == 'DELETE':
         orders.remove_order(order)
-        # for order_item in order_items:
-        #     if order_item.order_number == order_number:
-        #         order_items.remove(order_item)
         return "order " + order_number + " canceled!"
 
 
-# @app.route('/order/<order_number>/drink/<item_name>')
-# def add_item_to_order(order_number, item_name):
-#     # item_to_add = {}
-#     # for drink in menu_items["drinks"]:
-#     #     if drink.name == item_name:
-#     #         item_to_add = drink
-#     #         break
-
-#     # new_order_item = OrderItem(order_number, item_to_add)
-#     # order_items.append(new_order_item)
-#     return "item added!"
-
-
-@app.route('/order/<order_number>/drink', methods=['POST'])
+@app.route('/order/<order_number>/drink', methods=['POST', 'DELETE'])
 def add_drink_to_order(order_number):
     order = orders.find_order(int(order_number))
+    # Check if order exists
     if isinstance(order, str):
         return order
+    # Add drink if POST
     req_data = request.get_json()
     drink_name = req_data['name']
-    drink_price = req_data['price']
-    # drink = menu.find_drink(item_name)
-    order.add_item(Drink(drink_name, drink_price))
+    if request.method == 'POST':
+        drink_price = req_data['price']
+        order.add_item(Drink(drink_name, drink_price))
+        return str(drink_name) + " item added!"
+    # Remove drink if DELETE
+    elif request.method == 'DELETE':
+        order.remove_item(drink_name)
+        return str(drink_name) + " item removed!"
 
-    # drink_to_add = Drink(drink_name, drink_price)
 
-    # new_order_item = OrderItem(order_number, drink_to_add)
-    # order_items.append(new_order_item)
-    return str(drink_name) + " item added!"
-
-
-@app.route('/order/<order_number>/pizza', methods=['POST'])
+@app.route('/order/<order_number>/pizza', methods=['POST', 'DELETE'])
 def add_pizza_to_order(order_number):
     order = orders.find_order(int(order_number))
+    # Check if order exists
     if isinstance(order, str):
         return order
+    # Add pizza if POST
     req_data = request.get_json()
     pizza_name = req_data['name']
-    pizza_price = req_data['price']
-    order.add_item(Pizza(pizza_name, pizza_price))
-    # pizza_to_add = Pizza(pizza_name, pizza_price)
+    if request.method == 'POST':
+        pizza_price = req_data['price']
+        order.add_item(Pizza(pizza_name, pizza_price))
+        return str(pizza_name) + " item added!"
+    # Remove pizza if DELETE
+    elif request.method == 'DELETE':
+        order.remove_item(pizza_name)
+        return str(pizza_name) + " item removed!"
 
-    # new_order_item = OrderItem(order_number, pizza_to_add)
-    # order_items.append(new_order_item)
-    return str(pizza_name) + " item added!"
 
-
-@app.route('/order/<order_number>/custompizza', methods=['POST'])
+@app.route('/order/<order_number>/custompizza', methods=['POST', 'DELETE'])
 def add_custom_pizza_to_order(order_number):
     order = orders.find_order(int(order_number))
     # Check if order exists
     if isinstance(order, str):
         return order
-    # Otherwise add pizza
+    # Add custom pizza if POST
     req_data = request.get_json()
-    pizza_id = req_data['id']
-    pizza_size = req_data['size']
-    size = Size(pizza_size["name"], pizza_size["price"])
-    pizza_toppings = req_data['toppings']
-    toppings = []
-    for topping in pizza_toppings:
-        toppings.append(Topping(topping["name"], topping["price"]))
-    print(pizza_size)
-    print(pizza_toppings)
-    order.add_item(CustomPizza(pizza_id, size, toppings))
+    if request.method == 'POST':
+        pizza_id = order.custom_pizza_number
+        pizza_size = req_data['size']
+        size = Size(pizza_size["name"], pizza_size["price"])
+        pizza_toppings = req_data['toppings']
+        toppings = []
+        for topping in pizza_toppings:
+            toppings.append(Topping(topping["name"], topping["price"]))
+        order.add_item(CustomPizza(pizza_id, size, toppings))
 
-    return "custom pizza added!"
+        return "custom pizza added!"
+    # Remove custom pizza if DELETE
+    elif request.method == 'DELETE':
+        pizza_name = req_data['name']
+        order.remove_item(pizza_name)
+        return str(pizza_name) + " item removed!"
 
 
 if __name__ == "__main__":
